f7474287a0bb99f513eac67e140a8c754cb1e0a6
unknown <noogleindia@gmail.com>
2022-01-10 12:35:21 +0530

    Cloud Client Update

---
+++ src/Components/form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function form() {
+      return (
+            <div>
+                  <h3>Form</h3>
+
+                  <form action="http://localhost:3001/data" method="get">
+                        <label>Name: </label><br></br>
+                        <input type="text" name="name" id="" required /><br></br><br></br>
+                        <label>Phone: </label><br></br>
+                        <input type="number" name="phone" id="" required/><br></br><br></br>
+
+                        <button type='submit'>Submit</button>
+                  </form>
+                  
+            </div>
+      )
+}
